6fbc6d2a558a1516362a54cfe90f72dcf523a172
Fengwansheng <37141320+Fengwansheng@users.noreply.github.com>
2019-02-23 10:23:47 +0800

    做一次简单的修改
    加入属性name和password

--- test1.java
+++ test1.java
@@ -1,4 +1,12 @@
 class Demo{
+ private String name;
+ private String password;
+ public void setName(String name){
+ this.name=name;
+ }
+ public String getName(){
+ return name;
+ }
  private static void main(String[] args){
  System.out.println("你好");
  }
